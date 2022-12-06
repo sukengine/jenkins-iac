d4a7033f1a115561477300229d734d0cab33bbaa
sukengine <51234432+sukengine@users.noreply.github.com>
2022-12-06 10:47:59 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
+# Required
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key used to create infrastructure"
+}
+
+# Required
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key used to create AWS infrastructure"
+}
+
+
 variable "aws_region" {
   description = "The AWS region to create things in."
   default     = "ap-southeast-1"
